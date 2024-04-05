532510448ad334d41d0a12e868ac0677a6dafe69
Cristofer Hernández <crs.hdz.s@gmail.com>
2024-04-04 22:29:17 -0600

    Add recipe model and recipes data

---
+++ js/data/recipes.js
@@ -0,0 +1,15 @@
+// Ejemplo de recetas
+let recetas = [
+    new Receta("Ensalada César", "Desayuno", "Mezcla de lechuga con pollo y aderezo", [
+        { nombre: "lechuga", cantidad: 200, unidad: "g" },
+        { nombre: "pollo", cantidad: 150, unidad: "g" },
+        { nombre: "aderezo", cantidad: 50, unidad: "ml" }
+    ]),
+    new Receta("Pasta Alfredo", "Cena", "Pasta con salsa alfredo y pollo", [
+        { nombre: "pasta", cantidad: 300, unidad: "g" },
+        { nombre: "salsa_alfredo", cantidad: 200, unidad: "ml" },
+        { nombre: "pollo", cantidad: 100, unidad: "g" }
+    ]),
+    // Añadir más recetas según sea necesario
+    
+];
---
+++ js/models/recipe.js
@@ -0,0 +1,8 @@
+class Receta {
+    constructor(nombre, categoria, preparacion, ingredientes) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.preparacion = preparacion;
+        this.ingredientes = ingredientes;
+    }
+}
