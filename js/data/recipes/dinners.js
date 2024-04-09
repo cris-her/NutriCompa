4196e61cf0added6c9434042ca056f3e92829af7
cris-her <crs.hdz.s@gmail.com>
2024-04-08 22:35:43 -0600

    Implement generateInstructionsAndIngredients

--- js/data/recipes/dinners.js
+++ js/data/recipes/dinners.js
@@ -1,8 +1,8 @@
 let dinners = [
     new Recipe("Ensalada con lentejas", "Cena", "Rallar la zanahoria, cortar en cubos el pepino y revolver todos los ingredientes", [
-        { name: "Hojas verdes", portion: 2, unit: "taza" },
-        { name: "Lentejas", portion: 1/8, unit: "taza" },
-        { name: "Zanahoria", portion: 1/4, unit: "unit" },
+        { name: "Espinaca cruda", portion: 2, unit: "taza" },
+        { name: "Alubias, garbanzos, habas, lentejas cocidas", portion: 1/8, unit: "taza" },
+        { name: "Zanahoria Picada o rallada", portion: 1/4, unit: "taza" },
         { name: "Aguacate", portion: 1/8, unit: "unit" },
         { name: "Aceitunas", portion: 7, unit: "unit" },
         { name: "Limon", portion: 1/2, unit: "unit" },
@@ -10,7 +10,7 @@ let dinners = [
         { name: "Pepino", portion: 1/4, unit: "unit" }
     ]),
     new Recipe("Crema de zanahoria ", "Cena", "Poner a hervir dos tazas de agua, colocar las zanahorias en cubos durante 15 minutos, verter las zanahorias y el coco con una taza de agua ", [
-        { name: "Zanahoria", portion: 1/8, unit: "g" },
+        { name: "Zanahoria Picada o rallada", portion: 1/8, unit: "g" },
         { name: "Mantequilla de coco", portion: 1/16, unit: "taza" }
     ]),
     new Recipe("Sushi de surimi", "Cena", "", [
